23835df02a44d80a255c16e778c2c2aac87e926a
Tad Thorley <phaedryx@gmail.com>
2014-05-05 21:37:32 -0600

    more development

--- public/js/components.js
+++ public/js/components.js
@@ -1,27 +1,53 @@
 (function() {
-  var AddressAddForm, AddressBook, AddressList, div, form, h2, h3, input, label, span, table, tbody, td, th, thead, tr, _ref;
+  var AddressAddForm, AddressBook, AddressList, App, div, form, h2, h3, input, label, span, table, tbody, td, th, thead, tr, _ref;
 
   _ref = React.DOM, div = _ref.div, h2 = _ref.h2, h3 = _ref.h3, span = _ref.span, table = _ref.table, thead = _ref.thead, tbody = _ref.tbody, th = _ref.th, tr = _ref.tr, td = _ref.td, form = _ref.form, label = _ref.label, input = _ref.input;
 
+  AddressBook = (function() {
+    function AddressBook(addresses) {
+      this.addresses = addresses instanceof Array ? addresses : [];
+    }
+
+    AddressBook.prototype.addAddress = function(address) {
+      return this.addresses.push(address);
+    };
+
+    AddressBook.prototype.removeAddress = function(address) {
+      return this.addresses = this.addresses.filter(function(a) {
+        return a.key !== address.key;
+      });
+    };
+
+    return AddressBook;
+
+  })();
+
   AddressAddForm = React.createClass({
     submit: function() {
-      return alert("submitted");
+      var email, firstName, lastName, message, phone, _ref1;
+      _ref1 = this.refs, firstName = _ref1.firstName, lastName = _ref1.lastName, email = _ref1.email, phone = _ref1.phone;
+      message = "submitted:\n  first name: " + firstName.state.value + "\n  last name: " + lastName.state.value + "\n  email: " + email.state.value + "\n  phone: " + phone.state.value;
+      return alert(message);
     },
     render: function() {
       return form({
         onSubmit: this.submit
       }, input({
         type: 'text',
-        placeholder: 'first name'
+        placeholder: 'first name',
+        ref: 'firstName'
       }, input({
         type: 'text',
-        placeholder: 'last name'
+        placeholder: 'last name',
+        ref: 'lastName'
       }, input({
         type: 'text',
-        placeholder: 'email address'
+        placeholder: 'email address',
+        ref: 'email'
       }, input({
         type: 'text',
-        placeholder: 'phone number'
+        placeholder: 'phone number',
+        ref: 'phone'
       }, input({
         type: 'submit',
         className: 'btn btn-blue',
@@ -32,18 +58,39 @@
 
   AddressList = React.createClass({
     render: function() {
-      return table({}, thead({}, tr({}, th({}, "First Name"), th({}, "Last Name"), th({}, "Email Address"), th({}, "Phone Number"))));
+      var createRow;
+      createRow = function(address) {
+        return tr({}, td({}, "" + address.firstName), td({}, "" + address.lastName), td({}, "" + address.email), td({}, "" + address.phone), td({}, "remove link"));
+      };
+      return table({}, thead({}, tr({}, th({}, "First Name"), th({}, "Last Name"), th({}, "Email Address"), th({}, "Phone Number"), th({}, ""))), tbody({}, this.props.addressBook.addresses.map(createRow)));
     }
   });
 
-  AddressBook = React.createClass({
+  App = React.createClass({
+    getInitialState: function() {
+      var address;
+      address = {
+        key: 1,
+        firstName: "Bill",
+        lastName: "Jones",
+        email: "",
+        phone: ""
+      };
+      return {
+        addressBook: new AddressBook([address])
+      };
+    },
     render: function() {
       return div({
         id: "address-book"
-      }, h2({}, "Address Book"), AddressAddForm(), AddressList());
+      }, h2({}, "Address Book"), AddressAddForm({
+        addressBook: this.state.addressBook
+      }), AddressList({
+        addressBook: this.state.addressBook
+      }));
     }
   });
 
-  window.AddressBook = AddressBook;
+  window.App = App;
 
 }).call(this);
--- src/coffeescript/components.coffee
+++ src/coffeescript/components.coffee
@@ -4,21 +4,52 @@
   form, label, input
 } = React.DOM
 
+class AddressBook
+  constructor: (addresses) ->
+    @addresses = if (addresses instanceof Array) then addresses else []
+
+  addAddress: (address) ->
+    @addresses.push(address)
+  removeAddress: (address) ->
+    @addresses = @addresses.filter((a) -> a.key != address.key)
+
+
 AddressAddForm = React.createClass(
   submit: ->
-    alert("submitted")
+    {firstName, lastName, email, phone} = @refs
+    message = """
+      submitted:
+        first name: #{firstName.state.value}
+        last name: #{lastName.state.value}
+        email: #{email.state.value}
+        phone: #{phone.state.value}
+    """
+    alert(message)
 
   render: ->
     form {onSubmit: @submit},
-      input {type: 'text', placeholder: 'first name'},
-      input {type: 'text', placeholder: 'last name'},
-      input {type: 'text', placeholder: 'email address'},
-      input {type: 'text', placeholder: 'phone number'},
+      input {type: 'text', placeholder: 'first name',    ref: 'firstName'},
+      input {type: 'text', placeholder: 'last name',     ref: 'lastName'},
+      input {type: 'text', placeholder: 'email address', ref: 'email'},
+      input {type: 'text', placeholder: 'phone number',  ref: 'phone'},
       input {type: 'submit', className: 'btn btn-blue', value: 'Add'}
 )
 
 AddressList = React.createClass(
   render: ->
+    createRow = (address) ->
+      tr {},
+        td {},
+          "#{address.firstName}"
+        td {},
+          "#{address.lastName}"
+        td {},
+          "#{address.email}"
+        td {},
+          "#{address.phone}"
+        td {},
+          "remove link"
+
     table {},
       thead {},
         tr {},
@@ -29,16 +60,24 @@ AddressList = React.createClass(
           th {},
             "Email Address"
           th {},
-            "Phone Number"
+            "Phone Number",
+          th {},
+            ""
+      tbody {},
+        @props.addressBook.addresses.map(createRow)
 )
 
-AddressBook = React.createClass(
+App = React.createClass(
+  getInitialState: ->
+    address = {key: 1, firstName: "Bill", lastName: "Jones", email: "", phone: ""}
+    {addressBook: new AddressBook([address])}
+
   render: ->
     div {id: "address-book"},
       h2 {},
         "Address Book",
-      AddressAddForm(),
-      AddressList()
+      AddressAddForm({addressBook: @state.addressBook}),
+      AddressList({addressBook: @state.addressBook})
 )
 
-window.AddressBook = AddressBook
+window.App = App
